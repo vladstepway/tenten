117413637b503e1a4d212aaf2a54867ab93fbddc
ustsepavy <vladstepovoyvios@gmail.com>
2021-02-28 22:11:38 +0300

    feat: added figures factory

---
+++ src/utils/BlockFactory.js
@@ -0,0 +1,80 @@
+export default {
+    blockRules:
+        [
+            [   //small square
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            ],
+            [   //2 in a row
+                [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            ],
+            [   //3 in a row
+                [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            ],
+            [   //4 in a row
+                [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            ],
+            [   //5 in row
+                [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            ],
+            [   //long l figure
+                [0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            ],
+            [   //short l figure
+                [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            ],
+            [   //mid square
+                [0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            ],
+            [   //big square
+                [0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            ]
+        ],
+    index: 0,    //block index
+    status: 0,   //block status
+    getCurrentFigureColor(index) {
+        switch (index) {
+            case 0:
+                return "#7e8ed5";
+            case 1:
+                return "#ffc63e";
+            case 2:
+                return "#ed954a";
+            case 3:
+                return "#e76a82";
+            case 4:
+                return "#dc6555";
+            case 5:
+                return "#5cbee4";
+            case 6:
+                return "#59cb86";
+            case 7:
+                return "#98dc55";
+            case 8:
+                return "#4dd5b0";
+            default :
+                return "black";
+        }
+    },
+    generateFigure: function () {    //generate figure
+        this.index = Math.floor(Math.random() * this.blockRules.length);
+        this.status = Math.floor(Math.random() * this.blockRules[this.index].length);
+        return {
+            shape: this.getCurrentFigure(),
+            color: this.getCurrentFigureColor(this.index),
+            style: {}
+        };
+    },
+    getCurrentFigure: function () {
+        return this.blockRules[this.index][this.status];
+    }
+};

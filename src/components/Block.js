b5c40b6e68c06d33a555cde3b4b36ae462b0ae85
ustsepavy <vladstepovoyvios@gmail.com>
2021-02-28 22:24:37 +0300

    feat: added blocks components

---
+++ src/components/Block.js
@@ -0,0 +1,54 @@
+import React, {Component} from "react";
+
+class Block extends Component {
+    //Figure
+    constructor(props) {
+        super(props);
+        this.handleMouseDown = this.handleMouseDown.bind(this);
+    }
+
+    handleMouseDown(e) {
+        e.preventDefault()
+        console.log(e.target)
+        let moveHandler = e => {
+            //is dragging from app
+            let x = e.clientX
+            let y = e.clientY
+            this.props.isDrag(x, y);
+        };
+        document.onmousemove = moveHandler
+        document.ontouchmove = moveHandler
+        let upHandler = e => {
+            if (e.target.className !== ' cell') {//Pitted to death by blank space
+                return
+            }
+            //It’s not tied to Tetris because the z-index of the grid is on Tetris, so Tetris cannot listen to mouseup.
+            //Notify the end of the drag
+            this.props.isDrop();
+            document.onmousemove = null;
+        };
+        document.onmouseup = upHandler
+        document.ontouchend = upHandler
+    }
+
+    render() {
+        return (
+            <div
+                className="block_container"
+                onMouseDown={this.handleMouseDown}
+                onTouchStart={this.handleMouseDown}
+                style={this.props.block.style}
+            >
+                {this.props.block.shape.map((i, index) =>
+                    <div
+                        key={index}
+                        className="block_cell"
+                        style={{background: i ? this.props.block.color : "transparent"}}
+                    />
+                )}
+            </div>
+        );
+    }
+}
+
+export default Block;
---
+++ src/components/Blocks.js
@@ -0,0 +1,32 @@
+import React, {Component} from "react";
+import Block from "./Block";
+
+class Blocks extends Component {
+    //Figures
+    constructor(props) {
+        super(props);
+        this.handleDrag = this.handleDrag.bind(this);
+        this.handleDrop = this.handleDrop.bind(this);
+    }
+
+    handleDrag(i, x, y) {
+        this.props.onDrag(x, y, i);
+    }
+
+    handleDrop(i) {
+        this.props.onDrop(i)
+    }
+
+    render() {
+        return (
+            <div className="container">
+                {this.props.srcCells.map((blk, index) =>
+                    <Block key={index} isDrag={this.handleDrag.bind(this, index)}
+                           isDrop={this.handleDrop.bind(this, index)} block={blk}/>
+                )}
+            </div>
+        );
+    }
+}
+
+export default Blocks;

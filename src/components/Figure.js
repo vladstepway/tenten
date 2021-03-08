b5499d371a95aed067a87669211322880a3a564d
ustsepavy <vladstepovoyvios@gmail.com>
2021-03-08 22:28:14 +0300

    fix: fixed bug

--- src/components/Figure.js
+++ src/components/Figure.js
@@ -4,7 +4,6 @@ import clickSound from './../assets/sounds/click.mp3';
 
 export default function Figure(props) {
   //Figure
-  const [play] = useSound(clickSound);
   const useStyles = makeStyles((theme) =>
     createStyles({
       blockContainer: {
